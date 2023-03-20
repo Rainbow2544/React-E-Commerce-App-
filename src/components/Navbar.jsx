afe877a21c48c2ada26f66b9bb69dcd7a2fd3a4f
Rainbow <kenesou@gmail.com>
2023-03-21 00:16:38 +1300

    finish navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import './Navbar.css';
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 
 const Language = styled.span`
     font-size: 14px;
@@ -17,13 +18,32 @@ const Navbar = () => {
         <div className='left'>
             <Language>En</Language>
             <div className='searchContainer'>
-                <Search/>
-                
-                <ShoppingCartOutlined/>
+                <input 
+                    type="text"
+                    placeholder= "Enter your search here"
+                    className='nav-input'/>
+                <Search style={{ color: "gray", fontSize: 25 }}/>
+
             </div>
         </div>
-        <div className='center'>center</div>
-        <div className='right'>right</div>
+        <div className='center'>
+            <h1 id='logo'>LAMA.</h1>
+        </div>
+        <div className='right'>
+            <div className='menuItem'>
+                REGISTER
+            </div>
+            <div className='menuItem'>
+                SIGN IN
+            </div>
+            <div className='menuItem'>
+                <Badge badgeContent={4} color="primary">
+                    <ShoppingCartOutlined/>
+                </Badge>
+                
+            </div>
+            
+        </div>
         </div>
         
 
